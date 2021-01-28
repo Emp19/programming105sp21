b0154772555eb9c0065f541c177f26535f353aff
Emp19 <78036810+Emp19@users.noreply.github.com>
2021-01-28 11:39:34 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,31 @@
+#Assignment:
+#Copy your initial program from Programming Practice 1.1 into a new .py file. (Ths was the poem or song that you printed
+# to the screen.) Modify the file to accept input from the user, assign the input to a minimum of 8 variables, and use
+# the variables in the output. You are essentially creating a mad-libs game.
+
+marvel_hero = input("Who is your favorite Marvel hero?: ")
+num = input("What is your favorite number?: ")
+synonym = input("Synonym for run: ")
+cash = input("Another word for cash: ")
+rich = input("Another word for rich: ")
+body_Part = input("Name a body part: ")
+family = input("Name of family member: ")
+soda = input("Favorite soft drink: ")
+
+print("Aim at your " + body_Part + ", yeah, take off your " + body_Part +", yeah")
+print("I'm swingin' when I'm off the ecstasy (Uh), that's a Molly park, yeah")
+print("Iron on me, hoo-hoo, that's a "+ marvel_hero +", yeah")
+print("Pourin' fours in a " + str(num) + " ounce " + soda + ", yeah")
+print("I'm O.C, three-gram wood full of OG (Huh)")
+print("Give BM dick like Moby (Uh), gun make him " + synonym + ", Adobe (Uh)")
+print("Pay up that " + cash + ", you owe me, yeah, huh, b****, I need it")
+print("Matter fact, f*** that s***, I'm " + rich + ", uh, you can keep it")
+print("Yeah (B****, woo, yeah), damn")
+print("Rich n***** over here (They over, huh, here), yeah")
+print("Broke n***** over there (They over, uh, there, hoo)")
+print("Why is " + family + " over here? (Why?)")
+print("Go over there (Go over, uh, go over, hoo, go over there, what?)")
+print("Yeah, yeah, yeah, yeah)")
+
+
+
